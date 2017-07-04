4d6314147244570d79b9463e7ca7eaf9dc6e8015
g.nikoloudakis@pasiphae.eu <g.nikoloudakis@pasiphae.eu>
2017-07-04 12:02:00 +0300

    .

---
+++ Evaluation.py
@@ -0,0 +1,14 @@
+from Functions import *
+
+
+f = eFunctions()
+
+# print f.sendMessages(5, '127.0.0.1', '8088', 'a')
+# print f.sendMessages(10, '127.0.0.1', '8088', 'b')
+# print f.sendMessages(100, '127.0.0.1', '8088', 'c')
+# print f.sendMessages(1000, '127.0.0.1', '8088', 'd')
+
+# print f.sendMessages(5, '10.0.3.42', '8088', 'a')
+# print f.sendMessages(10, '10.0.3.42', '8088', 'b')
+# print f.sendMessages(100, '10.0.3.42', '8088', 'c')
+# print f.sendMessages(1000, '10.0.3.42', '8088', 'd')
